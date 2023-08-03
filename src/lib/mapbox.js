a45daaad08a7959cc8f941717c9734b359215ac0
Matt <79999977+MatthwFrost@users.noreply.github.com>
2023-08-03 14:17:15 +0100

    Update mapbox.js

--- src/lib/mapbox.js
+++ src/lib/mapbox.js
@@ -1,7 +1,7 @@
 import mapbox from 'mapbox-gl';
 
 // https://docs.mapbox.com/help/glossary/access-token/
-mapbox.accessToken = 'pk.eyJ1IjoibWF0dGh3ZnJvc3QiLCJhIjoiY2xqdnBwYnd3MGk1ZTN1bjE5NndlaGg5biJ9.c73X5BW_s2t3BfYCKX8cOQ';
+mapbox.accessToken = '';
 
 const key = {};
 
